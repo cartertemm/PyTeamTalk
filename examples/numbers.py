70b242ad9025881136727a1b58c535c741c13e73
Carter Temm <crtbraille@gmail.com>
2020-07-15 17:55:57 -0500

    Partial error handling and tab indentation in number facts example

--- examples/numbers.py
+++ examples/numbers.py
@@ -14,42 +14,50 @@ endpoint_url = "http://numbersapi.com/"
 
 
 def make_request(*args):
-    path = endpoint_url + "/".join(args)
-    try:
-        r = requests.get(path)
-        r.raise_for_status()
-    except Exception as exc:
-        return exc
-    else:
-        return r.text
+	path = endpoint_url + "/".join(args)
+	try:
+		r = requests.get(path)
+		r.raise_for_status()
+	except Exception as exc:
+		return str(exc)
+	else:
+		return r.text
 
 
 def trivia(number=None):
-    if not number:
-        number = "random"
-    return make_request(number, "trivia")
+	if not number:
+		number = "random"
+	elif number != "random" and not number.isdigit():
+		return "Numbers must be digits"
+	return make_request(number, "trivia")
 
 
 def math(number=None):
-    if not number:
-        number = "random"
-    return make_request(number, "math")
+	if not number:
+		number = "random"
+	elif number != "random" and not number.isdigit():
+		return "Numbers must be digits"
+	return make_request(number, "math")
 
 
 def date(number=None):
-    if not number:
-        number = "random"
-    return make_request(number, "date")
+	if not number:
+		number = "random"
+	elif number != "random" and number.count("/") != 1:
+		return "Dates must be in M/D format"
+	return make_request(number, "date")
 
 
 def year(number=None):
-    if not number:
-        number = "random"
-    return make_request(number, "year")
+	if not number:
+		number = "random"
+	elif not number != "random" and not number.isdigit():
+		return "Numbers must be digits"
+	return make_request(number, "year")
 
 
 def help():
-    return """Valid keywords: trivia, math, date or year vollowed by a number\rNumbers can be integers (duh), dates (month/day), or random for anything. if none is provided, random is assumed."""
+	return """Valid keywords: trivia, math, date or year vollowed by a number\rNumbers can be integers (duh), dates (month/day), or random for anything. if none is provided, random is assumed."""
 
 
 t = teamtalk.TeamTalkServer("example.com", 10333)
@@ -57,23 +65,23 @@ t = teamtalk.TeamTalkServer("example.com", 10333)
 
 @t.subscribe("messagedeliver")
 def message(server, params):
-    if params["type"] == teamtalk.USER_MSG:
-        content = params["content"].strip().lower().split(" ")
-        source = t.get_user(params["srcuserid"])
-        if content[0] == "trivia":
-            func = trivia
-        elif content[0] == "math":
-            func = math
-        elif content[0] == "date":
-            func = date
-        elif content[0] == "year":
-            func = year
-        else:
-            server.user_message(source, help())
-            return
-        server.user_message(source, func(*content[1:]))
+	if params["type"] == teamtalk.USER_MSG:
+		content = params["content"].strip().lower().split(" ")
+		source = t.get_user(params["srcuserid"])
+		if content[0] == "trivia":
+			func = trivia
+		elif content[0] == "math":
+			func = math
+		elif content[0] == "date":
+			func = date
+		elif content[0] == "year":
+			func = year
+		else:
+			server.user_message(source, help())
+			return
+		server.user_message(source, func(*content[1:]))
 
 
 t.connect()
-t.login("bot1", "admin", "password", "TeamTalkBotClient")
+t.login("number bot", "admin", "password", "TeamTalkBotClient")
 t.handle_messages(1)
